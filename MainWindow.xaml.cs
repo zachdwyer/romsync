e445deff559cc1b2cef0bcce5eab4af0ebc4a70a
zachdwyer <zakdwyer@outlook.com>
2020-02-08 00:22:07 -0500

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+using ListView = System.Windows.Controls.ListView;
+using MessageBox = System.Windows.MessageBox;
+using ToastNotifications;
+using ToastNotifications.Lifetime;
+using ToastNotifications.Position;
+using Application = System.Windows.Application;
+using ToastNotifications.Messages;
+
+namespace RomSync
+{
+    /*
+     * Load roms in input
+     * If roms in output
+     *      Compare each rom in input to roms in output. 
+     *      If rom matches, Rom.HasMatch. Else, Rom.NoMatch. Default: Rom.MatchingNotDone
+     *      
+     *      User selects multiple roms.
+     *      User can then sync those roms with the button. Show a progress bar.
+     */
+
+
+    public partial class MainWindow : Window
+    {
+        #region Variables
+
+        // Toast notification
+        Notifier notifier = new Notifier(cfg =>
+        {
+            // Window position
+            cfg.PositionProvider = new WindowPositionProvider(
+                parentWindow: Application.Current.MainWindow,
+                corner: Corner.BottomRight,
+                offsetX: 10,
+                offsetY: 10);
+
+            // Time to live
+            cfg.LifetimeSupervisor = new TimeAndCountBasedLifetimeSupervisor(
+                notificationLifetime: TimeSpan.FromSeconds(2.5),
+                maximumNotificationCount: MaximumNotificationCount.FromCount(1));
+
+            // Dispatcher that deploys the notification
+            cfg.Dispatcher = Application.Current.Dispatcher;
+        });
+
+
+
+
+        public string InputFileDirectory 
+        {
+            get { return (string)this.GetValue(InputFileDirectoryProperty); }
+            set { this.SetValue(InputFileDirectoryProperty, value); }
+        }
+        public static readonly DependencyProperty InputFileDirectoryProperty 
+            = DependencyProperty.Register("InputFileDirectory", typeof(string), typeof(MainWindow), new PropertyMetadata());
+
+        public string OutputFileDirectory
+        {
+            get { return (string)this.GetValue(OutputFileDirectoryProperty); }
+            set { this.SetValue(OutputFileDirectoryProperty, value); }
+        }
+        public static readonly DependencyProperty OutputFileDirectoryProperty 
+            = DependencyProperty.Register("OutputFileDirectory", typeof(string), typeof(MainWindow), new PropertyMetadata());
+
+        // Bound to ListViews
+        public ObservableCollection<Rom> InputRoms { get; set; }        
+        public ObservableCollection<Rom> OutputRoms { get; set; }
+
+        #endregion
+
+        public MainWindow()
+        {
+            // Initialize values
+            InputFileDirectory = "Z:\\ROMs\\vectrex";
+            OutputFileDirectory = "C:\\Users\\zakdw\\Desktop\\rom test";
+            InputRoms = new ObservableCollection<Rom>();
+            OutputRoms = new ObservableCollection<Rom>();
+            InitializeComponent();
+
+            populateRomList(InputFileDirectory, true);
+            populateRomList(OutputFileDirectory, false);
+
+            findMatchingRoms();
+
+        }
+
+        
+
+        private void populateRomList(string directory, bool useInputRoms)
+        {
+            // Clear applicable rom list
+            if (useInputRoms)
+            {
+                InputRoms.Clear();
+                Debug.WriteLine("Cleared input roms.");
+                FileInfo[] fileInfos = new DirectoryInfo(directory).GetFiles();
+                foreach (FileInfo f in fileInfos)
+                {
+                    InputRoms.Add(new Rom(f));
+                }
+
+                Debug.WriteLine("Input roms were added. A total of " + InputRoms.Count);
+
+            } else
+            {
+                OutputRoms.Clear();
+                Debug.WriteLine("Cleared output roms.");
+                FileInfo[] fileInfos = new DirectoryInfo(directory).GetFiles();
+                foreach (FileInfo f in fileInfos)
+                {
+                    OutputRoms.Add(new Rom(f));
+                }
+
+                Debug.WriteLine("Output roms were added. A total of " + OutputRoms.Count);
+            }
+        }
+
+        private void findMatchingRoms()
+        {
+            foreach (Rom r in InputRoms)
+            {
+                r.SyncStatus = 0;
+            }
+
+            foreach (Rom r in OutputRoms)
+            {
+                r.SyncStatus = 0;
+            }
+
+            IEnumerable<Rom> inputRomsMatchingOutputRoms = InputRoms.Intersect<Rom>(OutputRoms);
+
+            foreach(Rom match in inputRomsMatchingOutputRoms)
+            {
+                match.SyncStatus = 1;
+            }
+
+            IEnumerable<Rom> outputRomsMatchingInputRoms = OutputRoms.Intersect<Rom>(InputRoms);
+
+            foreach (Rom match in outputRomsMatchingInputRoms)
+            {
+                match.SyncStatus = 1;
+            }
+
+        }
+
+        private void SelectInputPathButton_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            DialogResult dr = folderBrowserDialog.ShowDialog();
+
+            if(dr == System.Windows.Forms.DialogResult.OK)
+            {
+                InputFileDirectory = folderBrowserDialog.SelectedPath;
+                populateRomList(InputFileDirectory, true);
+                populateRomList(OutputFileDirectory, false);
+                findMatchingRoms();
+            }
+        }
+
+        private void SelectOutputPathButton_Click(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            DialogResult dr = folderBrowserDialog.ShowDialog();
+
+            if (dr == System.Windows.Forms.DialogResult.OK)
+            {
+                OutputFileDirectory = folderBrowserDialog.SelectedPath;
+                populateRomList(OutputFileDirectory, false);
+                populateRomList(InputFileDirectory, true);
+                findMatchingRoms();
+            }
+        }
+
+        // InputRoms ListView has been clicked. 
+        private void InputRomsListView_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            bool enableButton = false;
+
+            if (InputRomsListView.SelectedItems != null && InputRomsListView.SelectedItems.Count > 0)
+            {
+                foreach (Rom r in InputRomsListView.SelectedItems)
+                {
+                    // Unsynced rom in the list?
+                    if (r.SyncStatus == 0)
+                    {
+                        // Enable the button.
+                        enableButton = true;
+                    }
+                }
+            }
+
+            SyncSelectedButton.IsEnabled = enableButton;
+
+        }
+
+        private void SyncSelectedButton_Click(object sender, RoutedEventArgs e)
+        {
+            int errorcount = 0;
+            ArrayList roms = new ArrayList(InputRomsListView.SelectedItems);
+            foreach (Rom r in roms)
+            {
+                try
+                {
+                    File.Copy(r.FileInfo.FullName, OutputFileDirectory + "\\" + r.FileInfo.Name, false);
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    ShowError("Can't access the file \"" + r.FileInfo.Name + "\" - this program doesn't have permission. Try running as administrator?");
+                    errorcount++;
+                }
+                catch(DirectoryNotFoundException)
+                {
+                    ShowError("Wasn't able to copy to that directory - it doesn't exist, at least this program thinks it doesn't. " +
+                        "Did you remove the storage media or did the network drive lose connection?");
+                    break;
+                }
+                catch(FileNotFoundException)
+                {
+                    ShowError("\"" + r.FileInfo.Name + "\" wasn't found. This program couldn't copy it. It may have been removed suddenly, the network drive" +
+                        "could have lost connection, or the storage media was suddenly removed.");
+                }
+                catch(IOException) 
+                { 
+                    // just ignore this one 
+                }
+            }
+
+            if (errorcount < 1)
+            {
+                notifier.ShowSuccess("Sync complete!");
+            } else
+            {
+                notifier.ShowWarning("Sync either partially complete or failed.");
+            }
+
+            populateRomList(OutputFileDirectory, false);
+            populateRomList(InputFileDirectory, true);
+            findMatchingRoms();
+
+        }
+
+        private void ShowError(string message)
+        {
+            MessageBox.Show(message, "RomSync Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        private void DeleteSelectedButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Are you sure you want to delete the selected ROMs from the output device?", "RomSync Message", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            
+            if (result == MessageBoxResult.Yes)
+            {
+                int errorcount = 0;
+                ArrayList roms = new ArrayList(OutputRomsListView.SelectedItems);
+                foreach (Rom r in roms)
+                {
+                    try
+                    {
+                        File.Delete(r.FileInfo.FullName);
+                    } 
+                    catch(UnauthorizedAccessException)
+                    {
+                        ShowError("Can't delete the file \"" + r.FileInfo.Name + "\" - this program doesn't have permission. Try running as administrator?");
+                        errorcount++;
+                    }
+                    catch (DirectoryNotFoundException)
+                    {
+                        ShowError("Wasn't able to delete from that directory - it doesn't exist, at least this program thinks it doesn't. " +
+                            "Did you remove the storage media or did the network drive lose connection?");
+                        break;
+                    }
+
+                }
+
+                if (errorcount < 1)
+                {
+                    notifier.ShowSuccess("File deletion complete.");
+                }
+                else
+                {
+                    notifier.ShowWarning("Delete either partially complete or failed.");
+                }
+                populateRomList(OutputFileDirectory, false);
+                populateRomList(InputFileDirectory, true);
+                findMatchingRoms();
+            }
+        }
+    }
+}
---
+++ Rom.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace RomSync
+{
+    public class Rom : INotifyPropertyChanged
+    {
+        // List of images available to set Icon to. Depends on the extension for the file.
+        private readonly static BitmapImage VectrexImage = new BitmapImage(new System.Uri("pack://application:,,,/Resources/vectrex.png"));
+        private readonly static BitmapImage UnknownImage = new BitmapImage(new System.Uri("pack://application:,,,/Resources/unknown.png"));
+        private readonly static BitmapImage NESImage = new BitmapImage(new System.Uri("pack://application:,,,/Resources/nes.png"));
+        private readonly static BitmapImage ZIPImage = new BitmapImage(new System.Uri("pack://application:,,,/Resources/ZIP.png"));
+        private readonly static BitmapImage WaitingImage = new BitmapImage(new System.Uri("pack://application:,,,/Resources/waiting.png"));
+        private readonly static BitmapImage GreenCheckImage = new BitmapImage(new System.Uri("pack://application:,,,/Resources/greencheck.png"));
+
+        // Information about the Rom - the name, extension, directory, etc. is all in this object.
+        public FileInfo FileInfo { get; set; }
+
+        // Returns the Icon of the image, depending on what the file extension is.
+        public BitmapImage Icon 
+        {
+            get
+            {
+                switch(FileInfo.Extension) 
+                {
+                    case (".vec"):
+                        return VectrexImage;
+                    case (".nes"):
+                    case (".fds"):
+                        return NESImage;
+                    case (".zip"):
+                        return ZIPImage;
+                    default:
+                        return UnknownImage;
+                }
+            }
+        }
+
+        // Returns the image that represents the sync status of this Rom.
+        public BitmapImage SyncStatusImage
+        {
+
+            get
+            {
+                switch (SyncStatus)
+                {
+                    case -1:
+                        return null;
+                    case 0:
+                        return WaitingImage;
+                    case 1:
+                        return GreenCheckImage;
+                    default:
+                        return null;
+                }
+            }
+
+        }
+
+        
+        private int syncStatus;
+
+        /// <summary>
+        /// -1 = unset, 0 = unsynced, 1 = synced
+        /// </summary>
+        public int SyncStatus
+        {
+            get 
+            {
+                return syncStatus;
+            }
+
+            set 
+            {
+                syncStatus = value;
+                if (syncStatus < -1 || syncStatus > 1)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                NotifyPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // This method is called by the Set accessor of each property.  
+
+        // The CallerMemberName attribute that is applied to propertyName  
+        // causes the property/method (in this case, property) name of the caller to used as the argument.  
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public override bool Equals(object x)
+        {
+            return x.GetHashCode() == this.GetHashCode();
+        }
+
+        public override int GetHashCode()
+        {
+            int hash = 7;
+            // Suitable nullity checks etc, of course :)
+            hash = hash + FileInfo.Name.GetHashCode();
+            return hash;
+        }
+
+        public Rom(FileInfo fileInfo)
+        {
+            FileInfo = fileInfo;
+            SyncStatus = -1;
+        }
+    }
+}
